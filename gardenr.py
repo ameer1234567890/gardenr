f1b61be0c206fc77981021c432c1cfe811922692
Ameer Dawood <ameer1234567890@gmail.com>
2019-01-02 23:10:42 +0500

    progress

--- gardenr.py
+++ gardenr.py
@@ -8,10 +8,10 @@ import ssl
 import time
 import datetime
 import multiprocessing
-#import RPi.GPIO as GPIO
 import os
 import json
 import I2C_LCD_driver
+import smbus
 
 PORT = 443
 PID_FILE = '/tmp/gardenr.pid'
@@ -68,14 +68,12 @@ if __name__ == '__main__':
     try:
         with open(PID_FILE, 'w') as fh:
             fh.write(str(os.getpid()))
-        run_server_thread = multiprocessing.Process(target=run_server)
-        run_server_thread.start()
+        #run_server_thread = multiprocessing.Process(target=run_server)
+        #run_server_thread.start()
         update_data_thread = multiprocessing.Process(target=update_data)
         update_data_thread.start()
     except:
-        run_server_thread.terminate()
+        #run_server_thread.terminate()
         update_data_thread.terminate()
-        #GPIO.cleanup()
-        #print('GPIO cleanup done!')
         os.remove(PID_FILE)
         print('PID file removed!')
