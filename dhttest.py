96d58caa4e7f23fcb156d233a64e67bc30c577de
Ameer Dawood <ameer1234567890@gmail.com>
2019-01-03 14:16:02 +0500

    progress

---
+++ dhttest.py
@@ -0,0 +1,13 @@
+#!/usr/bin/sudo env/bin/python3
+# *-* coding: utf-8 -*-
+
+import Adafruit_DHT
+import time
+
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 4
+
+dht_humidity, dht_temp = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+print('DHT Temperature: ' + dht_temp + '°C DHT Humidity: ' + dht_humidity + '%'
+print('DHT Temperature: {0:0.1f}°C DHT Humidity: {1:0.1f}%'.format(dht_temp, dht_humidity))
+time.sleep(1)
