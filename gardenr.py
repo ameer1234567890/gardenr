bf533f438eac42696871ad17fd81bd91d5f633ab
Ameer Dawood <ameer1234567890@gmail.com>
2019-01-18 23:46:51 +0500

    take a break

--- gardenr.py
+++ gardenr.py
@@ -88,7 +88,6 @@ class HTTPSHandler(http.server.SimpleHTTPRequestHandler):
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
                 self.wfile.write('Posted'.encode('utf-8'))
-                update_data()
             else:
                 self.send_response(400)
                 self.send_header('Content-type', 'text/html')
