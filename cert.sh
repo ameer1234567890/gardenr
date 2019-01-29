e0b0d32649d3556d3502b8e9534e431be77cdbcb
Ameer Dawood <ameer1234567890@gmail.com>
2019-01-30 01:26:36 +0500

    Fix certificate generation and renewal

--- cert.sh
+++ cert.sh
@@ -8,13 +8,15 @@ fi
 systemctl stop gardenr.service
 
 if [ -d /etc/letsencrypt/live/gardenr.ameer.io ]; then
-  sudo certbot renew --standalone
+  sudo certbot renew --standalone --renew-hook "echo 'YES' > updated.txt"
 else
   sudo certbot certonly --standalone -d gardenr.ameer.io -m ameer1234567890@gmail.com --agree-tos
 fi
 
-cat /etc/letsencrypt/live/gardenr.ameer.io/cert.pem > gardenr.pem
-cat /etc/letsencrypt/live/gardenr.ameer.io/privkey.pem >> gardenr.pem
-mv -f gardenr.pem /etc/letsencrypt/live/gardenr.ameer.io
+if [ "$(cat updated.txt 2>/dev/null)" = "YES" ]; then
+  cat /etc/letsencrypt/live/gardenr.ameer.io/cert.pem > gardenr.pem
+  cat /etc/letsencrypt/live/gardenr.ameer.io/privkey.pem >> gardenr.pem
+  mv -f gardenr.pem /etc/letsencrypt/live/gardenr.ameer.io
+fi
 
 systemctl start gardenr.service
