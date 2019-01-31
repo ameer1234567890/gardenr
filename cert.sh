53b01434bacf4205b0b39c0f459a0b753543d92f
Ameer Dawood <ameer1234567890@gmail.com>
2019-01-31 14:47:38 +0500

    Fix exit status

--- cert.sh
+++ cert.sh
@@ -8,17 +8,17 @@ fi
 systemctl stop gardenr.service
 
 if [ -d /etc/letsencrypt/live/gardenr.ameer.io ]; then
-  sudo certbot renew --standalone --renew-hook "echo 'YES' > updated.txt"
+  sudo certbot renew --standalone --renew-hook "sh -c 'exit 100'"
+  status="$?"
+  if [ "$status" -eq 0 ]; then
+    status="0"
+  fi
 else
   sudo certbot certonly --standalone -d gardenr.ameer.io -m ameer1234567890@gmail.com --agree-tos
   status="$?"
-  if [ "$status" = "0" ]; then
-    echo 'YES' > updated.txt
-  fi
 fi
 
-if [ "$(cat updated.txt 2>/dev/null)" = "YES" ]; then
-  rm updated.txt
+if [ "$status" -eq 0 ]; then
   cat /etc/letsencrypt/live/gardenr.ameer.io/cert.pem > gardenr.pem
   cat /etc/letsencrypt/live/gardenr.ameer.io/privkey.pem >> gardenr.pem
   mv -f gardenr.pem /etc/letsencrypt/live/gardenr.ameer.io
